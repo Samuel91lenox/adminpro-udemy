6dd7a10c44f03582a0f5f500d9ffaba452b59c9e
U-INDRA\sgonzalezga <sgonzalezga@sgonzalezga10.indra.es>
2018-04-06 13:34:41 +0200

    Seccion 8 Terminada

--- src/app/shared/breadcrums/breadcrums.component.ts
+++ src/app/shared/breadcrums/breadcrums.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivationEnd } from '@angular/router';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 @Component({
   selector: 'app-breadcrums',
   templateUrl: './breadcrums.component.html',
@@ -7,7 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumsComponent implements OnInit {
 
-  constructor() { }
+  pagina:string = '';
+
+  constructor(private router: Router,public _title:Title,public _meta:Meta) {
+   
+    this.getDataRoute().subscribe(data => {
+        console.log(data);
+        this.pagina=data.titulo;
+        this._title.setTitle(this.pagina);
+
+        let metaTag:MetaDefinition={
+          name: 'description',
+          content: this.pagina
+        };
+        this._meta.updateTag(metaTag);
+    });
+   }
+
+     getDataRoute(){
+      return this.router.events
+      .filter(event=>event instanceof ActivationEnd)
+      .filter((event:ActivationEnd)=>event.snapshot.firstChild===null)
+      .map((event:ActivationEnd) => event.snapshot.data);
+     }
+
+
+
 
   ngOnInit() {
   }
